23a8017a8ec08aa676932fb61f18b4266c84b54e
Vidhya1605 <145219484+Vidhya1605@users.noreply.github.com>
2023-09-25 17:46:02 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+ #factorial of a number using recursion
+def recur_factorial(n):
+ if n==1:
+    return n
+ else:
+    return n*recur_factorial(n-1)
+num=int(input("Enter a number:"))
+print("The factorial of", num, "is",recur_factorial(num))
